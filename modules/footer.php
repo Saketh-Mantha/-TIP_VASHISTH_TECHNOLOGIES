41cad37588a8815b2cfc87aba9530a45485071a1
Saketh Mantha <sakethcr7mantha@gmail.com>
2022-02-08 01:53:36 -0800

    ATTENDANCE SYSTEM

---
+++ modules/footer.php
@@ -0,0 +1,47 @@
+</div>
+		<footer id="footer">
+			<div id="">
+			
+
+			
+				
+			</p>
+			</div>
+			<div id="links">
+			Developer: 
+				<a href="#" taget="_blank">Saketh Mantha </a> 
+				 
+			</div>
+			
+		</footer>
+		<style>
+			footer {
+			position: fixed;
+  			bottom: 0;
+  			background-color: black;
+  			width: 100%;
+  			text-align: center; 
+  			color:beige;
+  			padding-bottom: 10px;
+			  border: 5px solid black;
+
+			}	
+			
+
+			#footer a{
+				margin-right:10px;
+				text-decoration: none;
+				color: beige;
+				padding-left:15px;
+				padding-right:15px;
+				margin-bottom:10px;
+			}
+			footer a:hover{
+				color: white;
+			}
+
+		</style>
+		<script src="js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
---
+++ modules/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Student Attendance Record System</title>
+    
+    <link href="css/bootstrap.min.css" rel="stylesheet">   
+		<link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+		<script src="js/jquery.min.js"></script>
+		<script src="js/validator.min.js"></script>
+  </head>
+  <body>
+  
+  	<div id="wrapper">
+    <div class="overlay"></div>
+    
+    <h1><i class="glyphicon glyphicon-calender"></i>  Student Attendance Record System</h1>
+    <style>
+      h1{
+        margin-top:0;
+        padding: 20px;
+        background-color: black;
+        color: beige;
+        text-align: center;
+        font-family: 'Lobster', cursive;
+        font-size:50px
+      }
+      .sidebar-wrapper{
+        color: white;
+      }
+     
+    </style>
